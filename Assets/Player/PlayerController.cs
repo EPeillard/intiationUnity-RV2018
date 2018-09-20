8aa0087df02ec4035dcd7136e25f0c9e2934019d
Etienne Peillard <etienne.peillard@ec-nantes.fr>
2018-09-20 11:59:32 +0200

    First commit

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    // Use this for initialization
+    void Start () {
+        if (!isLocalPlayer)
+        {
+            gameObject.GetComponentInChildren<Camera>().enabled = false;
+            gameObject.GetComponentInChildren<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = false;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+            return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            CmdFire();
+        }
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 50;
+
+        // Spawn the bullet on the Clients
+        NetworkServer.Spawn(bullet);
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+}
